d47736a7c3044dfada13f5da045ad4d6360fafda
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-06-14 19:20:22 -0400

    _

--- chatAppReact/chat-app/src/server/index.js
+++ chatAppReact/chat-app/src/server/index.js
@@ -1,11 +1,11 @@
 var app = require('http').createServer()
-var io = module.export.io = require('socket.io')(app)
+var io = module.exports.io = require('socket.io')(app)
 
 const PORT = process.env.PORT || 3231
 
-const SocketManager = require('./SocketManger');
+const SocketManager = require('./SocketManager');
 
-io.connection('connection', SocketManager)
+io.on('connection', SocketManager)
 
 app.listen(PORT, ()=>{
   console.log('Connect to port:'+ PORT);
