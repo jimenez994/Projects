7816f97466a6e8e3cb0f332a8430ed6cfb87f115
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-07-09 17:10:14 -0400

    _

---
+++ eclipseWS/proBolivia/src/main/java/com/zeus/proBolivia/ProBoliviaApplication.java
@@ -0,0 +1,12 @@
+package com.zeus.proBolivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProBoliviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProBoliviaApplication.class, args);
+	}
+}
